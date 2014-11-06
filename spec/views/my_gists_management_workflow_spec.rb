e5682b945a5e80879ec742d8119d0647b311cdb5
John <john@futureworkz.com>
2014-11-06 15:53:05 +0700

    add my gists management workflow

---
+++ spec/views/my_gists_management_workflow_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'Create a gist' do 
+  it 'allows user to create a gist' do 
+    visit new_my_gist_url
+    fill_in 'Title', with: 'Gist 1'
+    fill_in 'Content', with: 'Gist 1 content'
+    click_on 'Submit'
+    expect(page).to have_content 'Gist created successfully.'
+  end
+end
