e167ffc1168fd0d55371a49d20fff13279d83aa4
John <john@futureworkz.com>
2014-11-07 10:18:31 +0700

    rename MyGistsView#new to SnippetsView#new

--- spec/views/snippet_management_workflow_spec.rb
+++ spec/views/snippet_management_workflow_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe 'Create a gist' do 
+describe 'Create a snippet' do 
   it 'allows user to create a gist' do 
-    visit new_my_gist_url
-    fill_in 'Title', with: 'Gist 1'
-    fill_in 'Content', with: 'Gist 1 content'
-    click_on 'Create My gist'
-    expect(page).to have_content 'Gist created successfully.'
+    visit new_snippet_url
+    fill_in 'Title', with: 'Snippet 1'
+    fill_in 'Content', with: 'Snippet 1 content'
+    click_on 'Create Snippet'
+    expect(page).to have_content 'Snippet created successfully.'
   end
 end
