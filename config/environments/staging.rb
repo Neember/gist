bde7324e4f3ac8142dbecfac21d044ac830d389c
John <john@futureworkz.com>
2014-11-13 09:03:27 +0700

    update environments

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -78,12 +78,9 @@ Rails.application.configure do
 
 	# for devise
 	config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
-
 	config.action_mailer.delivery_method = :smtp
-
 	config.action_mailer.smtp_settings = {
 		address:              "smtp.gmail.com",
-		domain:               "gmail.com",
 		authentication:       "plain",
 		user_name:            "neember.gist",
 		password:             "123456a1@",
