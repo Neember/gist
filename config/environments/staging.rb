c44aa0b48fb17d5938b68266cdb26ec97430cb9c
John <john@futureworkz.com>
2014-11-13 14:01:46 +0700

    update staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
 	# config.log_tags = [ :subdomain, :uuid ]
 
 	# Use a different logger for distributed setups.
-	# config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+	config.logger = Logger.new(STDOUT)
 
 	# Use a different cache store in production.
 	# config.cache_store = :mem_cache_store
@@ -87,4 +87,5 @@ Rails.application.configure do
     port:                 587,
     enable_starttls_auto: true
   }
+  
 end
