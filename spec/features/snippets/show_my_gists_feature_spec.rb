c82502d2f5f5b5cc3a1a508162c470587be246a6
John <john@futureworkz.com>
2014-11-14 09:04:04 +0700

    update show_snippet_listing_feature and add show_my_gists_feature

---
+++ spec/features/snippets/show_my_gists_feature_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Show My Gists listing' do
+  let!(:setup_rails)    { create(:snippet) }
+  let!(:paperclip)      { create(:snippet) }
+
+  it 'displays a list of snippets' do
+    login_as(setup_rails.user, :scope => :user)
+
+    visit my_gists_snippets_url
+    expect(page).to have_content setup_rails.title
+    expect(page).not_to have_content paperclip.title
+    
+    expect(page).to have_link('Edit',   { href: edit_snippet_path(setup_rails) })
+    expect(page).not_to have_link('Edit',   { href: edit_snippet_path(paperclip) })
+
+    expect(page).to have_link('Delete',   { href: snippet_path(setup_rails) })
+    expect(page).not_to have_link('Delete',   { href: snippet_path(paperclip) })
+
+    expect(page).to have_link('Detail', { href: snippet_path(setup_rails) })
+    expect(page).not_to have_link('Detail', { href: snippet_path(paperclip) })
+  end
+end
--- spec/features/snippets/show_snippet_listing_feature_spec.rb
+++ spec/features/snippets/show_snippet_listing_feature_spec.rb
@@ -6,23 +6,20 @@ describe 'Show snippet listing' do
 
   it 'displays a list of snippets' do
     visit snippets_url
+    
     expect(page).not_to have_link('Edit',   { href: edit_snippet_path(setup_rails) })
     expect(page).not_to have_link('Delete',   { href: snippet_path(setup_rails) })
     expect(page).not_to have_content('Gist Management')
+
     login_as(setup_rails.user, :scope => :user)
     visit snippets_url
     expect(page).to have_content('Gist Management')
-    expect(page).to have_link('My Gists',  { href: snippets_path })
+    expect(page).to have_link('My Gists',  { href: my_gists_snippets_path })
     expect(page).to have_link('Create a gist', { href: new_snippet_path })
     expect(page).to have_content setup_rails.title
-    expect(page).not_to have_content paperclip.title
+    expect(page).to have_content paperclip.title
     expect(page).to have_link('Edit',   { href: edit_snippet_path(setup_rails) })
-    expect(page).not_to have_link('Edit',   { href: edit_snippet_path(paperclip) })
-
     expect(page).to have_link('Delete',   { href: snippet_path(setup_rails) })
-    expect(page).not_to have_link('Delete',   { href: snippet_path(paperclip) })
-
     expect(page).to have_link('Detail', { href: snippet_path(setup_rails) })
-    expect(page).not_to have_link('Detail', { href: snippet_path(paperclip) })
   end
 end
