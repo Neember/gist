608f862394aeea10bb76ce68e78d355591766e0f
John <john@futureworkz.com>
2014-11-15 06:52:02 +0700

    add snippet_file_factory

--- spec/factories/snippet_file_factory.rb
+++ spec/factories/snippet_file_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do 
   factory :snippet_file do 
     sequence(:name) { |n| "File #{n}" }
+    content 'This is the content of the file'
     type 'text/plain'
     language 'Markdown'
     association :snippet
