c75a573edd7980c1ecbd2a3dbb42b5772ca4e727
John <john@futureworkz.com>
2014-11-15 06:54:56 +0700

    update services/snippet_puller_specs

--- spec/services/snippet_puller_spec.rb
+++ spec/services/snippet_puller_spec.rb
@@ -15,10 +15,29 @@ describe SnippetPuller do
   describe '#gists' do
     let(:user) { create(:user, username: 'Johnytran')}
 
-    it 'return gists of user' do
+    it 'returns gists of user' do
       puller = SnippetPuller.new(user)
       gist_data = puller.gists
       expect(gist_data).to_not eq nil
+    end
+  end
+
+  describe '#gist' do 
+    let(:code) { '55599a17e859093bc085' }
+    
+    it 'return gist info' do
+      gist_info = SnippetPuller.gist(code)
+      expect(gist_info['id']).to eq code
+    end
+  end
+
+  describe '#pull' do
+    let!(:user) { create(:user, username: 'Johnytran') }
+    let!(:code) { '55599a17e859093bc085' }
+
+    it 'pulls the gist to snippet' do
+      ##puller = SnippetPuller.new(user)
+      ##expect(puller.pull(code)).to change(Snippet, :count).by(1)
     end
   end
 end
