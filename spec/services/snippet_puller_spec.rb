b732c567898363d33595277032280a72c392eed7
John <john@futureworkz.com>
2014-11-14 14:32:51 +0700

    Add snippet_puller_spec

--- spec/services/snippet_puller_spec.rb
+++ spec/services/snippet_puller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'stubs/github_api'
 
 describe SnippetPuller do 
   describe '#user_data' do 
@@ -8,6 +9,16 @@ describe SnippetPuller do
       puller = SnippetPuller.new(user)
       user_data = puller.user_data
       expect(user_data['login']).to eq user.username
+    end
+  end
+
+  describe '#gists' do
+    let(:user) { create(:user, username: 'Johnytran')}
+
+    it 'return gists of user' do
+      puller = SnippetPuller.new(user)
+      gist_data = puller.gists
+      expect(gist_data).to_not eq nil
     end
   end
 end
