7c47e6d27bcea10dd2c5bb518f22aed61be6e0fa
John <john@futureworkz.com>
2014-11-16 16:38:41 +0700

    update snippet model: include nested attributes and rails admin config

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,4 +1,5 @@
 class Snippet < ActiveRecord::Base
+  include RailsAdmin::Snippet
   attr_accessor :email
 
   belongs_to :user
@@ -8,6 +9,8 @@ class Snippet < ActiveRecord::Base
   has_many :tags, through: :tagables, :source => :tag
 
   validates :title,   presence: true
+
+  accepts_nested_attributes_for :snippet_files
 
   enum status: ['Personal', 'Share']
 
