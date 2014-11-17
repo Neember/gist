b01e8fea3bd5963d2ea86ea54602db232f200a1a
John <john@futureworkz.com>
2014-11-17 12:01:07 +0700

    add authenticate user create to GithubApisController

--- app/controllers/github_apis_controller.rb
+++ app/controllers/github_apis_controller.rb
@@ -1,8 +1,18 @@
 class GithubApisController < ApplicationController
-  before_filter :authenticate_user!, only: [:gists]
+  before_filter :authenticate_user!, only: [:gists, :create]
 
   def gists
     puller = SnippetPuller.new(current_user)
     @gists = puller.gists
+  end
+
+  def create
+    
+  end
+
+  private
+
+  def code
+    params.require(:code)
   end
 end
