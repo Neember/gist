11aeff7859095f9b17aeae780db5e86a6626427a
John <john@futureworkz.com>
2014-11-17 10:53:50 +0700

    update show_snippet_feature: remove snippet file

--- spec/features/snippets/show_snippet_feature_spec.rb
+++ spec/features/snippets/show_snippet_feature_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe 'display :show view' do
   let!(:snippet) { create(:snippet) }
-  let!(:snippet_file) { create(:snippet_file, snippet: snippet )}
 
   it 'shows a snippet' do
     expect(page).not_to have_link('Edit',   { href: edit_snippet_path(snippet) })
@@ -12,11 +11,8 @@ describe 'display :show view' do
     visit snippet_url(snippet)
 
     expect(page).to have_content(snippet.title)
-    expect(page).to have_content(snippet_file.name)
 
     expect(page).to have_link('Edit',   { href: edit_snippet_path(snippet) })
     expect(page).to have_link('Delete',   { href: snippet_path(snippet) })
-    expect(page).to have_link('Add more file',   { href: new_snippet_snippet_file_path(snippet) })
-
   end
 end
