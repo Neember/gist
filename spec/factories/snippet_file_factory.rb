9da6e47e7398f28b8961dc645df386b9eb6e307d
John <john@futureworkz.com>
2014-11-17 10:28:24 +0700

    remove SnippetFileFactory

--- spec/factories/snippet_file_factory.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do 
-  factory :snippet_file do 
-    sequence(:name) { |n| "File #{n}" }
-    content 'This is the content of the file'
-    language 'Markdown'
-    association :snippet
-  end
-end
