8144bde2779fbe705ad737f0793360d1c9b0c47f
John <john@futureworkz.com>
2014-11-18 17:06:18 +0700

    update db seed: add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
 puts "Start seeding Admin"
 Admin.delete_all
-Admin.create! email: 'john@example.com', password: '123123123', password_confirmation: '123123123'
+Admin.create! email: 'stevenyap@futureworkz.com', password: '123123123', password_confirmation: '123123123'
+Admin.create! email: 'jack@futureworkz.com', password: '123123123', password_confirmation: '123123123'
+Admin.create! email: 'john@futureworkz.com', password: '123123123', password_confirmation: '123123123'
 puts "Finish seeding Admin"
 
 puts 'Start seeding User'
+
 User.delete_all
-User.create! email: 'john@futureworkz.com', password: '123123123', password_confirmation: '123123123'
+
+usernames = ['john', 'martin', 'jack', 'stevenyap', 'frank', 'khoa', 'maicon', 'seika', 'anthony']
+
+usernames.each do |username|
+  User.create! email: "#{username}@futureworkz.com", password: '123123123', password_confirmation: '123123123'
+end
+puts "Finish seeding User"
+
+puts 'Start seeding tags'
+Tag.create! title: 'CSS'
+Tag.create! title: 'Ruby'
+Tag.create! title: 'Ruby on Rails'
+Tag.create! title: 'Javascript'
+Tag.create! title: 'Refactor'
+Tag.create! title: 'SCSS'
+Tag.create! title: 'SOLID'
+Tag.create! title: 'CoffeeScript'
+Tag.create! title: 'Git'
 puts "Finish seeding User"
