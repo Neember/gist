2b2e72370a0d6e590261c2b8f3242a8381328b85
Jack A. Huang <jack@futureworkz.com>
2014-11-21 11:32:30 +0700

    Fix markdown highlight when pygment lexers are not available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 class HtmlWithPygments < Redcarpet::Render::HTML
+  def check_language_available(language)
+    Pygments.lexers.select { |key, lexer| lexer[:aliases].include? language }.present?
+  end
+
   def block_code(code, language)
-    Pygments.highlight(code, lexer: language, options: {linespans: 'line'})
+    options = {options: {linespans: 'line'}}
+    options.merge!(lexer: language) if check_language_available(language)
+    Pygments.highlight(code, options)
   end
 end
 
@@ -9,5 +15,4 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(HtmlWithPygments, fenced_code_blocks: true, disable_indented_code_blocks: true)
     markdown.render(text).html_safe
   end
-
 end
